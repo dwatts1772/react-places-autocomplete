468d7f6cc54a9a838eb2eedf5c3c22da7bebdf67
Mike Piccolo <mfpiccolo@gmail.com>
2018-03-11 23:46:52 -0700

    add dist

---
+++ dist/defaultStyles.js
@@ -0,0 +1,33 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var defaultStyles = {
+  root: {
+    position: "relative",
+    paddingBottom: "0px"
+  },
+  input: {
+    display: "inline-block",
+    width: "100%"
+  },
+  autocompleteContainer: {
+    position: "absolute",
+    top: "100%",
+    backgroundColor: "white",
+    border: "1px solid #555555",
+    width: "100%"
+  },
+  autocompleteItem: {
+    backgroundColor: "#ffffff",
+    padding: "10px",
+    color: "#555555",
+    cursor: "pointer"
+  },
+  autocompleteItemActive: {
+    backgroundColor: "#fafafa"
+  }
+};
+
+exports.default = defaultStyles;
